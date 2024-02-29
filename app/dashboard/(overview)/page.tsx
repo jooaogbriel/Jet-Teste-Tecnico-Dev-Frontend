6eacca9544759269b8fec1f7966fc18d5f578fcf
João Gonçalves <joao_i0807@outlook.com>
2024-02-29 15:17:42 -0300

    debbug: concertando tipagem de children

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,10 @@
 "use client"
 
 import Hero from "@/app/ui/dashboard/hero";
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <div>
         <Hero />
