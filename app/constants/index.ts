4e0581c11d7a21dbcb864992f62fd26d762933a6
João Gonçalves <joao_i0807@outlook.com>
2024-02-29 14:58:29 -0300

    rocket: go to deploy

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,37 +1,64 @@
+import porshe from '@/app/images/porshe.jpg'
+import ferrari from '@/app/images/ferrari.jpeg'
+import lambo  from '@/app/images/lambo.webp'
+import mc from '@/app/images/mc.jpg'
+import aston from '@/app/images/aston.jpeg'
+import buggati from '@/app/images/bugatti.jpeg'
+import tesla from '@/app/images/tesla.jpeg'
+
 export const cars = [
     {
+        img: porshe,
         name: "Porsche 911",
         nextReservation: "30/03",
-        status: true,
+        status: "Avaliable",
+        rating: 3
+        
     },
     {
-        name: "Ferrari 488 GTB",
+        img: ferrari,
+        name: "Ferrari 488 SPIDER",
         nextReservation: "11/03",
-        status: false,
+        status: "Avaliable",
+        rating: 5
+        
     },
     {
+        img: lambo,
         name: "Lamborghini Huracan",
         nextReservation: "12/03",
-        status: false,
+        status: "Avaliable",
+        rating: 5
+       
     },
     {
+        img:mc,
         name: "McLaren 720S",
         nextReservation: "12/03",
-        status: true,
+        status: "Avaliable",
+        rating: 2
+       
     },
     {
+        img:aston,
         name: "Aston Martin DB11",
         nextReservation: "13/03",
-        status: true,
+        status: "Avaliable",
+        rating: 5
+  
     },
     {
+        img: buggati,
         name: "Bugatti Chiron",
         nextReservation: "13/03",
-        status: false,
+        status: "Avaliable",
+        rating: 3
     },
     {
+        img: tesla,
         name: "Tesla Model S",
         nextReservation: "14/03",
-        status: true,
+        status: "Avaliable",
+        rating: 4
     }
 ];
